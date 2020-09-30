87ae9ab85d08384b2b2565b1551bee9a1e45a1cc
Artyom Somov <avsomov7@gmail.com>
2020-09-30 22:47:45 +0300

    Added catalog parser Small fixes

--- src/main/java/com/wine/to/up/winelab/parser/service/controller/ParserController.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/controller/ParserController.java
@@ -32,16 +32,16 @@ public class ParserController {
     MessageRepository messageRepository;
 
     @GetMapping
-    public void parseProductPage(@RequestParam Integer productID) {
+    public void parseProductPage(@RequestParam int productID) {
 
-        String url = "https://www.winelab.ru/product/" + productID;
+        final String url = "https://www.winelab.ru/product/" + productID;
 
         Wine resultWine = new Wine();
         try {
             Document document = Jsoup.connect(url).get();
 
-            final String patternVolumeInMilliliters = "(?i)\\d*([.,]\\d+)? ?([мМ][лЛ])";
-            final String patternVolumeInLiters = "(?i)\\d*([.,]\\d+)? ?[лЛ]";
+            final String patternVolumeInMilliliters = "\\d+ ?([мМ][лЛ])";
+            final String patternVolumeInLiters = "\\d*([.,]\\d+)? ?[лЛ]";
             final String patternAlcoholPercentage = "\\d{0,2}([.,]\\d+)? ?%";
             final int millilitersInLiter = 1000;
             final int centsInRubles = 100;
@@ -55,47 +55,51 @@ public class ParserController {
 
             for (Element filter: document.select("div.product_description div.filters > span")){
                 String tag = filter.ownText();
-                Integer volume;
-                BigDecimal alcoholPerecentage;
+                int volume;
+                BigDecimal alcoholPercentage;
 
                 if (tag.matches(patternVolumeInMilliliters)){
-                    tag = tag.replaceAll("[ мМлЛ]", "").replaceAll(",", ".");
-                    volume = (int) (Double.parseDouble(tag));
+                    tag = tag.replaceAll("[ мМлЛ]", "");
+                    volume = Integer.parseInt(tag);
                     log.info("Volume: " + volume + " ml");
                     resultWine.setVolume(BigDecimal.valueOf(volume));
                 }
-                if (tag.matches(patternVolumeInLiters)){
+                else if (tag.matches(patternVolumeInLiters)){
                     tag = tag.replaceAll("[ лЛ]", "").replaceAll(",", ".");
                     volume = (int) (Double.parseDouble(tag) * millilitersInLiter);
                     log.info("Volume: " + volume + " ml");
+                    resultWine.setVolume(BigDecimal.valueOf(volume));
                 }
                 else if (tag.matches(patternAlcoholPercentage)){
                     tag = tag.replaceAll("[ %]", "").replaceAll(",", ".");
-                    alcoholPerecentage = new BigDecimal(Double.parseDouble(tag));
-                    log.info("Alcohol percentage: " + alcoholPerecentage + "%");
-                    resultWine.setAlcoholPercentage(alcoholPerecentage);
+                    alcoholPercentage = new BigDecimal(Double.parseDouble(tag));
+                    log.info("Alcohol percentage: " + alcoholPercentage + "%");
+                    resultWine.setAlcoholPercentage(alcoholPercentage);
                 }
                 else{
                     log.info("Tag: " + tag);
                 }
             }
 
-            String oldPrice = document.select("div.product_description div.prices_main").first().ownText();
+            int oldPrice = Integer.parseInt(document.select("div.product_description div.prices_main").first().ownText().replaceAll(" ", "")) * 100;
+            resultWine.setPrice(oldPrice);
 
-            log.info("Old price: " + oldPrice);
+            log.info("Old price: " + oldPrice + " cents");
 
             Map<Integer, Integer> prices = new HashMap<>();
-            for(Element element: document.select("div.prices_cart_price")){
-                Integer quantity = Integer.parseInt(element.select("span").get(0).html().replaceAll("[x шт]", ""));
-                Integer price = (int) (Double.parseDouble(element.select("span").get(1).ownText().replaceAll(" ", "")) * 100);
+            for (Element element: document.select("div.prices_cart_price")){
+                int quantity = Integer.parseInt(element.select("span").get(0).html().replaceAll("[x шт]", ""));
+                int price = (int) (Double.parseDouble(element.select("span").get(1).ownText().replaceAll(" ", "")) * 100);
                 prices.put(quantity, price);
                 log.info("Price for " + quantity + " item(s): " + price + " cents");
-                resultWine.setPrice(price);
+                if(quantity == 1) {
+                    resultWine.setPrice(price);
+                }
             }
 
         }
         catch (IOException ex){
-            ex.printStackTrace();
+            log.info("Error: couldn't load the web page.");
         }
 
         log.info("result: " + resultWine);
@@ -104,28 +108,63 @@ public class ParserController {
     @GetMapping("/home")
     public void parseHomePage() {
 
-        String url = "https://www.winelab.ru/";
+        final String url = "https://www.winelab.ru/";
 
         try {
             Document document = Jsoup.connect(url).get();
 
             Elements elementsWithId = document.select("a.product_card.js-product-click");
-            List<Integer> ids = new ArrayList<Integer>();
+            List<Integer> ids = new ArrayList<>();
             for(Element item: elementsWithId) {
                 String idStr = item.attr("data-id");
-                Integer id = Integer.parseInt(idStr);
+                int id = Integer.parseInt(idStr);
                 if(ids.contains(id)) continue;
                 ids.add(id);
             }
 
-            for(Integer id: ids) {
+            for(int id: ids) {
                 this.parseProductPage(id);
             }
 
         }
         catch (IOException ex){
-            ex.printStackTrace();
+            log.info("Error: couldn't load the web page.");
         }
     }
 
+    @GetMapping("/catalog")
+    public void parseCatalog() {
+
+        log.info("Started parsing");
+
+        String url = "https://www.winelab.ru/catalog/vino";
+        boolean isLastPage = false;
+        List<Integer> ids = new ArrayList<>();
+
+        while (!isLastPage) {
+            try {
+                Document document = Jsoup.connect(url).get();
+
+                Elements productCards = document.select("div.container a.product_card");
+                for (Element element: productCards){
+                    ids.add(Integer.parseInt(element.attr("data-id")));
+                }
+
+                Element nextPage = document.select("ul.pagination li.page-item a[rel=next]").first();
+                if (nextPage == null) {
+                    isLastPage = true;
+                }
+                else {
+                    url = "https://www.winelab.ru" + nextPage.attr("href");
+                }
+
+            } catch (IOException ex) {
+                isLastPage = true;
+                log.info("Error: couldn't load the web page.");
+            }
+
+        }
+
+        log.info(ids.toString());
+    }
 }
--- src/main/java/com/wine/to/up/winelab/parser/service/dto/Wine.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/dto/Wine.java
@@ -1,14 +1,10 @@
 package com.wine.to.up.winelab.parser.service.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Wine {
@@ -25,49 +21,4 @@ public class Wine {
     //Price in cents
     private Integer price;
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public void setVolume(BigDecimal volume) {
-        this.volume = volume;
-    }
-
-    public void setAlcoholPercentage(BigDecimal alcoholPercentage) {
-        this.alcoholPercentage = alcoholPercentage;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setSugarConcentration(String sugarConcentration) {
-        this.sugarConcentration = sugarConcentration;
-    }
-
-    public void setGrapeSortId(Integer grapeSortId) {
-        this.grapeSortId = grapeSortId;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Wine{" +
-                "brand='" + brand + '\'' +
-                ", country='" + country + '\'' +
-                ", volume=" + volume +
-                ", alcoholPercentage=" + alcoholPercentage +
-                ", color='" + color + '\'' +
-                ", sugarConcentration='" + sugarConcentration + '\'' +
-                ", grapeSortId=" + grapeSortId +
-                ", price=" + price +
-                '}';
-    }
 }
