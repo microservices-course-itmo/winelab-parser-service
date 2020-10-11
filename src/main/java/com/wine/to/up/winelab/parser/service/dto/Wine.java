f51065def6c716668cd7b05be826526fbb59f0d8
cptbronzebeard <omg4itep@gmail.com>
2020-10-12 01:15:25 +0300

    Moved enums declaration and all the translation logic out of Wine.

--- src/main/java/com/wine/to/up/winelab/parser/service/dto/Wine.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/dto/Wine.java
@@ -1,9 +1,10 @@
 package com.wine.to.up.winelab.parser.service.dto;
 
+import com.wine.to.up.winelab.parser.service.utils.enums.Color;
+import com.wine.to.up.winelab.parser.service.utils.enums.Sugar;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.util.Map;
 
 @AllArgsConstructor
 @Getter
@@ -33,37 +34,9 @@ public class Wine {
     // is wine sparkling
     private boolean sparkling;
     //should enums really be private? We can't access any methods that way.
-    private enum Color{
-        RED, ROSE, WHITE
-    }
-    @Getter(AccessLevel.NONE)
-    @Setter(AccessLevel.NONE)
-    private Map<String, Color> colorMap = Map.of("красное", Color.RED, "розовое",
-            Color.ROSE, "белое", Color.WHITE, "светлое", Color.WHITE);
-    @Getter(AccessLevel.NONE)
-    @Setter(AccessLevel.NONE)
-    private Map<String, Sugar> sugarMap = Map.of("брют", Sugar.DRY, "сухое", Sugar.DRY,
-            "полусухое", Sugar.MEDIUM_DRY, "полусладкое", Sugar.MEDIUM, "сладкое", Sugar.SWEET);
     private Color color;
-    public Color setColor(String value) {
-        this.color = colorMap.get(value.toLowerCase());
-        return this.color;
-    }
-    private enum Sugar{
-        DRY, MEDIUM_DRY, MEDIUM, SWEET
-    }
-    Sugar sugar;
-    public Sugar setSugar(String value) {
-        this.sugar = sugarMap.get(value.toLowerCase());
-        return this.sugar;
-    }
+    private Sugar sugar;
     private String grapeSort;
     private String description;
     private String gastronomy;
-    public int getColorValue() {
-        return this.color.ordinal();
-    }
-    public int getSugarValue() {
-        return this.sugar.ordinal();
-    }
 }
