f17245998108f1decfcc6fbb32d9b0add12870df
Alexandr <a.pashkevich22@gmail.com>
2020-10-13 00:51:10 +0300

    feat: updateJob

---
+++ src/main/java/com/wine/to/up/winelab/parser/service/job/UpdateWineLabJob.java
@@ -0,0 +1,29 @@
+package com.wine.to.up.winelab.parser.service.job;
+
+import com.wine.to.up.winelab.parser.service.services.UpdateService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class UpdateWineLabJob {
+
+    @Autowired
+    private UpdateService updateService;
+
+    /**
+     * Каждый день обновляет список вин
+     */
+    @Scheduled(fixedRate = 24*60*60*1000)
+    public void runJob() {
+        long startDate = new Date().getTime();
+        log.info("start UpdateWineLabJob run job method at " + startDate);
+        updateService.updateCatalog();
+        log.info("end UpdateWineLabJob run job method at " + new Date().getTime() + " duration = " + (new Date().getTime() - startDate));
+    }
+
+}
---
+++ src/main/java/com/wine/to/up/winelab/parser/service/services/UpdateService.java
@@ -0,0 +1,36 @@
+package com.wine.to.up.winelab.parser.service.services;
+
+import com.wine.to.up.commonlib.messaging.KafkaMessageSender;
+import com.wine.to.up.winelab.parser.service.dto.Wine;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@Slf4j
+public class UpdateService {
+    @Autowired
+    private KafkaService kafkaService;
+    @Autowired
+    private ParserService parserService;
+
+    public void updateCatalog() {
+        try {
+            List<Integer> ids = parserService.parseCatalogs();
+            ids.forEach(id -> {
+                try {
+                    Wine wine = parserService.parseProduct(id);
+                    kafkaService.sendWine(wine);
+                } catch (IOException e) {
+                    log.error("Parse product with id = {} error, {}", id, e.toString());
+
+                }
+            });
+        } catch(IOException e) {
+            log.error("Update catalog error, {}",e.toString());
+        }
+    }
+}
