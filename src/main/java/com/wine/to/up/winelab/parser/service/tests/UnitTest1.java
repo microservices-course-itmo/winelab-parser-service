e22554ba7908901449f6fefdda3b365f82179f76
ComradAlex <komrad.alex3002@yandex.ru>
2020-10-13 14:12:40 +0300

    fixes

--- src/main/java/com/wine/to/up/winelab/parser/service/tests/UnitTest1.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/tests/UnitTest1.java
@@ -1,23 +1,24 @@
 package com.wine.to.up.winelab.parser.service.tests;
 
+import com.wine.to.up.winelab.parser.service.services.ParserService;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class UnitTest1 {
     @Test
     public static void testParser(String[] args) {
+        ParserService parserService = new ParserService();
+        parserService.parseProduct(1009581);
 
-        Wine testWine = ParserService.parseProduct("1009581");
-
-        Assert.assertNotNull(testWine.name);           //test the fields are not null/null (depends on the field)
-        Assert.assertNull(testWine.site);
-        Assert.assertNotNull(testWine.link);
-        Assert.assertNotNull(testWine.image);
-        Assert.assertNotNull(testWine.manufacturer);
-        Assert.assertNotNull(testWine.brand);
-        Assert.assertNotNull(testWine.country);
-        Assert.assertNull(testWine.region);
-        Assert.assertNotNull(testWine.description);
+        Assert.assertNotNull(parserService.parseProduct(1009581).name);           //test the fields are not null/null (depends on the field)
+        Assert.assertNull(parserService.parseProduct(1009581).site);
+        Assert.assertNotNull(parserService.parseProduct(1009581).link);
+        Assert.assertNotNull(parserService.parseProduct(1009581).image);
+        Assert.assertNotNull(parserService.parseProduct(1009581).manufacturer);
+        Assert.assertNotNull(parserService.parseProduct(1009581).brand);
+        Assert.assertNotNull(parserService.parseProduct(1009581).country);
+        Assert.assertNull(parserService.parseProduct(1009581).region);
+        Assert.assertNotNull(parserService.parseProduct(1009581).description);
 
     }
 }
--- src/main/java/com/wine/to/up/winelab/parser/service/tests/UnitTest2.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/tests/UnitTest2.java
@@ -1,5 +1,6 @@
 package com.wine.to.up.winelab.parser.service.tests;
 
+import com.wine.to.up.winelab.parser.service.services.ParserService;
 import org.junit.Test;
 import org.junit.Assert;
 
@@ -7,21 +8,23 @@ public class UnitTest2 {
     @Test
     public static void testParser(String[] args) {
 
-        Wine testWine = ParserService.parseProduct("1009581");
+        ParserService parserService = new ParserService();
+        parserService.parseProduct(1009581);
 
-        Assert.assertEquals(testWine.name) = "Вино Berton Foundstone Shiraz красное сухое 0,75 л";             //test the fields are being parsed correctly
-        Assert.assertEquals(testWine.site) = null;
-        Assert.assertEquals(testWine.oldPrice) = 750;
-        Assert.assertEquals(testWine.link) = "https://www.winelab.ru/product/1009581";
-        Assert.assertEquals(testWine.newPrice) = 675;
-        Assert.assertEquals(testWine.image) = "https://www.winelab.ru/medias/1009583.png-300Wx300H?context=bWFzdGVyfGltYWdlc3w0MDQwN3xpbWFnZS9wbmd8aW1hZ2VzL2g0Ny9oZmUvODgzMjYxODEzNTU4Mi5wbmd8NzJlOWYzYjJkMjNiNjc2ZTMzMDNiMjMyNTUxZWJkYWJlODBkYWZlMDI1MjAzMDE2NTYyMTAyYTU3ODVlNThkOQ";
-        Assert.assertEquals(testWine.manufacturer) = "Berton Vineyards";
-        Assert.assertEquals(testWine.brand) = "Berton Vinyard Reserve";
-        Assert.assertEquals(testWine.country) = "Австралия";
-        Assert.assertEquals(testWine.region) = null;
-        Assert.assertEquals(testWine.volume) = 0.75;
-        Assert.assertEquals(testWine.alcoholPercentage) = 13;
-        Assert.assertEquals(testWine.description) = "Отличается непревзойдённым сортовым ароматом с гармонично переплетающимися тонами миндаля, ванили и спелой вишни. Вкус бархатистый, с мягкой танинностью., gastronomy=Отлично подходит к мясу, приготовленному на открытом огне, а также копчёностям и колбасам. Температура подачи 16–18°С)";
+
+        Assert.assertEquals(parserService.parseProduct(1009581).name) = "Вино Berton Foundstone Shiraz красное сухое 0,75 л";             //test the fields are being parsed correctly
+        Assert.assertEquals(parserService.parseProduct(1009581).site) = null;
+        Assert.assertEquals(parserService.parseProduct(1009581).oldPrice) = 750;
+        Assert.assertEquals(parserService.parseProduct(1009581).link) = "https://www.winelab.ru/product/1009581";
+        Assert.assertEquals(parserService.parseProduct(1009581).newPrice) = 675;
+        Assert.assertEquals(parserService.parseProduct(1009581).image) = "https://www.winelab.ru/medias/1009583.png-300Wx300H?context=bWFzdGVyfGltYWdlc3w0MDQwN3xpbWFnZS9wbmd8aW1hZ2VzL2g0Ny9oZmUvODgzMjYxODEzNTU4Mi5wbmd8NzJlOWYzYjJkMjNiNjc2ZTMzMDNiMjMyNTUxZWJkYWJlODBkYWZlMDI1MjAzMDE2NTYyMTAyYTU3ODVlNThkOQ";
+        Assert.assertEquals(parserService.parseProduct(1009581).manufacturer) = "Berton Vineyards";
+        Assert.assertEquals(parserService.parseProduct(1009581).brand) = "Berton Vinyard Reserve";
+        Assert.assertEquals(parserService.parseProduct(1009581).country) = "Австралия";
+        Assert.assertEquals(parserService.parseProduct(1009581).region) = null;
+        Assert.assertEquals(parserService.parseProduct(1009581).volume) = 0.75;
+        Assert.assertEquals(parserService.parseProduct(1009581).alcoholPercentage) = 13;
+        Assert.assertEquals(parserService.parseProduct(1009581).description) = "Отличается непревзойдённым сортовым ароматом с гармонично переплетающимися тонами миндаля, ванили и спелой вишни. Вкус бархатистый, с мягкой танинностью., gastronomy=Отлично подходит к мясу, приготовленному на открытом огне, а также копчёностям и колбасам. Температура подачи 16–18°С)";
 
 
     }
