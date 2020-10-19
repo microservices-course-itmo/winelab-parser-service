aef3dbcbfe76b20b3b8fd72e66e067fb1d8bbbdf
Alexandr <a.pashkevich22@gmail.com>
2020-10-19 19:36:20 +0300

    fix: no db required

--- src/main/java/com/wine/to/up/winelab/parser/service/configuration/GeneralPurposesBeansConfig.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/configuration/GeneralPurposesBeansConfig.java
@@ -3,10 +3,13 @@ package com.wine.to.up.winelab.parser.service.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wine.to.up.winelab.parser.service.services.ParserService;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class GeneralPurposesBeansConfig {
 
     /**
