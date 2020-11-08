8c3513f884084f7f406eac5ca19f74d73d0ca221
Alexandr <a.pashkevich22@gmail.com>
2020-11-08 23:11:13 +0300

    fix: WineLabParserMetricsCollector constrictor

--- src/main/java/com/wine/to/up/winelab/parser/service/components/WineLabParserMetricsCollector.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/components/WineLabParserMetricsCollector.java
@@ -11,4 +11,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class WineLabParserMetricsCollector extends CommonMetricsCollector {
+    public WineLabParserMetricsCollector() {
+        super("winelab");
+    }
 }
