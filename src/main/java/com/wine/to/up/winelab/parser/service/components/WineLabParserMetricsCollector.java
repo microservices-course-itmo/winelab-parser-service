55818bdab6acb1f6687e42f51ba537959bd2af69
Artyom Somov <avsomov7@gmail.com>
2020-11-13 15:38:50 +0300

    cross review

--- src/main/java/com/wine/to/up/winelab/parser/service/components/WineLabParserMetricsCollector.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/components/WineLabParserMetricsCollector.java
@@ -1,4 +1,5 @@
-package com.wine.to.up.winelab.parser.service.components;
+//TODO create-service: move classes to correct package. F.e. for order-service all classes should be in "com.wine.to.up.order.service.*"
+package com.wine.to.up.demo.service.components;
 
 import com.wine.to.up.commonlib.metrics.CommonMetricsCollector;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
  * Prometheus' metrics exposed at /metrics-prometheus
  *
  */
+//TODO create-service: rename
 @Component
-public class WineLabParserMetricsCollector extends CommonMetricsCollector {
+public class DemoServiceMetricsCollector extends CommonMetricsCollector {
 }
