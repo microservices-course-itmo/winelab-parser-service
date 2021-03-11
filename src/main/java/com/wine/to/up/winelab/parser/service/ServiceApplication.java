0dedaf2af49dea698f9b089fa325e5923f1e1a01
Stepan <stepan210699@gmail.com>
2021-03-11 16:46:08 +0300

    initialize eureka

--- src/main/java/com/wine/to/up/winelab/parser/service/ServiceApplication.java
+++ src/main/java/com/wine/to/up/winelab/parser/service/ServiceApplication.java
@@ -2,10 +2,12 @@ package com.wine.to.up.winelab.parser.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("com.wine.to.up")
 @EnableSwagger2
